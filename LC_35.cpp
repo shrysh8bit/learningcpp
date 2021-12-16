0dc7fae249dc13ee74e752cbf972e4cfe185ea5c
shrysh8bit <shrysh1701@gmail.com>
2021-12-16 12:05:52 +0530

    search insert position

---
+++ LC_35.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int left = 0, right = nums.size() - 1;
+        int mid;
+        
+        while ( left <= right){
+            mid = ( left + right)/2;
+            if (nums[mid] == target) return mid;
+            
+            if (nums[mid] > target){
+                right = mid - 1;
+            }else{
+                left = mid + 1;
+            }
+        }
+        
+        return left;
+    }
+};
