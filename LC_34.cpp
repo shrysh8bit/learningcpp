af3b6517c2efc7c7d914cca36f94fb4aea0bc813
shrysh8bit <shrysh1701@gmail.com>
2021-12-22 21:26:39 +0530

    Update LC_34.cpp

--- LC_34.cpp
+++ LC_34.cpp
@@ -1,70 +1,80 @@
-class Solution {
-public:
-    vector<int> posn;
-    
-    void linear_search(vector<int>& nums, int mid){
-        int target = nums[mid], temp = mid;
-        int min_index = mid, max_index = mid;
-        
-        
-        
-        while (++mid < nums.size() ){
-            if(nums[mid] == target ){
-                max_index = mid;
-            }else{
-                break;
-            }
-        }
-         while(--temp >= 0){
-            if (nums[temp] == target){
-                min_index = temp;
-            }else{
-                break;
-            }
-         }
-        
-        posn.push_back(min_index);
-        posn.push_back(max_index);
-        
-        
-    }
-        
-    
-    void binary_search (vector<int>& nums, int target, int left, int right){
-        
-        while (left <= right){
-            
-            int mid = (left + right)/2;
-            
-            if (nums[mid] == target){
-                
-                linear_search(nums, mid);
-            }
-            
-            if(target < nums[mid]){
-                right = mid - 1;
-            }else{
-                left = mid +1;
-            }
-            
-            }
-       }
-   
-                   
-                   
-    
-    vector<int> searchRange(vector<int>& nums, int target) {
-        binary_search(nums, target, 0 , nums.size() -1);
-        
-        
-        if (posn.size() > 1){
-            sort(posn.begin(), posn.end());
-            posn.erase(posn.begin() + 1, posn.end() -1);
-        }else{            
-            posn.push_back(-1);
-            posn.push_back(-1);
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+vector<int> ans;
+
+
+void binary_left(vector<int>& nums, int target) {
+     int index = -1;
+     int left = 0, right = nums.size() -1;
+
+     while(left < right){
+        int mid = (left + right)/2;
+
+        if(target < nums[mid]){
+            right = mid - 1;
+        }else if (nums[mid] < target){
+            left = mid + 1;
         }
         
-        return posn;
+        if(nums[mid] == target){
+            index = mid;
+            right = mid - 1;
+        }
+     }
+
+    if (left == right && nums[left] == target) index = left;
+
+    ans.push_back(index);
+}
+
+void binary_right(vector<int>& nums, int target) {
+    
+    int index = -1;
+    int left = 0, right = nums.size() -1;
+
+    while(left < right){
+        int mid = (left + right)/2;
+
+        if(target < nums[mid]){
+            right = mid - 1;
+        }else if (nums[mid] < target){
+            left = mid + 1;
+        }
+        
+        if(nums[mid] == target){
+            index = mid;
+            left = mid + 1;
+        }
     }
-};
+
+    if (left == right && nums[left] == target) index = left;
+
+    ans.push_back(index);
+
+}
+
+
+vector<int> searchRange(vector<int>& nums, int target) {
+    
+    binary_left(nums, target);
+
+    binary_right(nums, target);
+    
+
+    return ans;
+}
+
+
+int main(){
+
+    vector<int> nums {};
+    int target = 1;
+
+    searchRange(nums, target);
+
+
+    return 0;
+}
