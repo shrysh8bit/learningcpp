6f84e41aaa9fc74be22c3cb9447d26c17d9474e2
shrysh8bit <shrysh1701@gmail.com>
2022-05-01 15:59:15 +0530

    Update LC_977.cpp

--- LC_977.cpp
+++ LC_977.cpp
@@ -1,16 +1,78 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        int a= nums.size();
         
-        vector<int> result(a, 0);
+        // case 1 : all nums positive
         
-        for ( int i = 0 ; i < a; i++){
-            result[i] = (nums[i]*nums[i]);
+        if (nums.front() >= 0 && nums.back() >= 0){
+            
+           for (int itr = 0; itr < nums.size(); itr++){
+               nums.at(itr) *= nums.at(itr); 
+           }
+        return nums;
+        
         }
         
-        sort(result.begin(), result.end());
         
-        return result;
+        //case 2  : all nums negative
+        
+        if (nums.front() < 0 && nums.back() < 0){
+            reverse (nums.begin(), nums.end());
+            
+            for (int itr = 0; itr < nums.size(); itr++){
+               nums.at(itr) *= nums.at(itr); 
+           }
+        return nums;
+        }
+        
+        
+        //case 3 : +ve and -ve mixed
+        
+        int fwd_itr, reverse_itr;
+        
+        for (int itr = 0 ; itr < nums.size(); itr++){
+            if (nums.at(itr) >= 0){
+                fwd_itr = itr;
+                reverse_itr = itr - 1;
+                break;
+            }
+        }
+        
+        vector<int> abs_sorted_vec;
+        int nums_size = nums.size();
+        
+        while (fwd_itr < nums_size || reverse_itr >= 0){
+            
+            if (fwd_itr < nums_size && reverse_itr >= 0){
+                if ((nums.at(fwd_itr)) <= abs(nums.at(reverse_itr)) ){
+                    
+                    abs_sorted_vec.push_back(nums.at(fwd_itr));
+                    fwd_itr++;
+                }else{
+                    
+                    abs_sorted_vec.push_back(abs(nums.at(reverse_itr)));
+                    reverse_itr--;
+                }
+            }
+
+            else if (fwd_itr >= nums_size){
+                abs_sorted_vec.push_back(abs(nums.at(reverse_itr)));
+                reverse_itr--;
+            }
+
+            else if (reverse_itr < 0){
+                abs_sorted_vec.push_back(abs(nums.at(fwd_itr)));
+                fwd_itr++;
+            }
+            
+        }
+        
+       for (int itr = 0; itr < abs_sorted_vec.size(); itr++){
+               abs_sorted_vec.at(itr) *= abs_sorted_vec.at(itr); 
+       }
+
+        return abs_sorted_vec;
+        
+        
     }
 };
