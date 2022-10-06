9e9ac3d82bbd1f7746e72e573c29ceff1363d7ef
shrysh1701@gmail.com <shrysh1701@gmail.com>
2022-10-07 00:36:29 +0530

    reverse bits

---
+++ LC_190.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t ans = 0;
+
+        for (int i = 0; i < 32; i++){
+            ans = (n&1) + ans;
+            if(i != 31) ans = ans << 1;
+            n = n>>1;
+        }
+
+        return ans;
+    }
+};
